7c1aa9b35d2dbd5a5fc101543b051dd1362333c8
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-22 18:39:43 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,31 +1,31 @@
 # Make your shoe class here!
 class Book
-    # def initialize(title)
-    #     @title = title
-    # end
-    # def title
-    #     @title
-    # end
-    # def author= (author)
-    #     @author = author
-    # end
-    # def author
-    #     @author
-    # end
-    # def page_count= (num)
-    #     @page_count = num
-    # end
-    # def page_count
-    #     @page_count
-    # end
-    # def genre=(genre)
-    #     @genre = genre
-    # end
-    # def genre
-    #     @genre
-    # end
+    def initialize(title)
+        @title = title
+    end
+    def title
+        @title
+    end
+    def author= (author)
+        @author = author
+    end
+    def author
+        @author
+    end
+    def page_count= (num)
+        @page_count = num
+    end
+    def page_count
+        @page_count
+    end
+    def genre=(genre)
+        @genre = genre
+    end
+    def genre
+        @genre
+    end
 
-    attr_accessor :tittle, :author, :page_count, :genre
+    # attr_accessor :tittle, :author, :page_count, :genre
     def turn_page
         puts "Flipping the page...wow, you read fast!"
     end
